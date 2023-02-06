5ebd761ba1e112b39e029f0b3bb49d90698cd3ad
Sam Cutter <samuelcutter27@gmail.com>
2023-02-06 12:10:47 +0000

    Added command line argument parsing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    work: u16,
+    rest: u16,
+    sessions: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Args::parse();
+
+    let work: &u16 = &args.work;
+    let rest: &u16 = &args.rest;
+    let sessions: &u8 = &args.sessions;
+
+    println!("You will work {sessions} sessions, each containing {work} minutes of work, and {rest} minutes of rest.")
 }
