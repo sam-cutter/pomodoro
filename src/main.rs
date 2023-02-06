d083f072aad584408b525e56692ddc83ef884a4c
Sam Cutter <samuelcutter27@gmail.com>
2023-02-06 13:30:59 +0000

    Added basic countdown

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::{thread, time};
 
 #[derive(Parser)]
 struct Args {
@@ -10,15 +11,25 @@ struct Args {
 fn main() {
     let args: Args = Args::parse();
 
-    let work: &u16 = &args.work;
-    let rest: &u16 = &args.rest;
+    let work_minutes: &u16 = &args.work;
+    let rest_minutes: &u16 = &args.rest;
     let sessions: &u8 = &args.sessions;
 
-    let work_seconds: u16 = work * 60;
-    let rest_seconds: u16 = rest * 60;
-    let session_seconds: u16 = work_seconds + rest_seconds;
+    let work_seconds: u16 = work_minutes * 60;
+    let rest_seconds: u16 = rest_minutes * 60;
+    // let session_seconds: u16 = work_seconds + rest_seconds;
 
-    println!("You will work {sessions} sessions, each containing {work} minutes of work, and {rest} minutes of rest.");
-    println!("You will work {sessions} sessions, each containing {work_seconds} seconds of work, and {rest_seconds} seconds of rest.");
-    println!("Each session will be {session_seconds} seconds long.")
+    for session in 1..=*sessions {
+        println!("Session {session}");
+
+        for work_seconds_left in (0..=work_seconds).rev() {
+            println!("There are {work_seconds_left} seconds left of working.");
+            thread::sleep(time::Duration::from_secs(1));
+        }
+
+        for rest_seconds_left in (0..=rest_seconds).rev() {
+            println!("There are {rest_seconds_left} seconds left of resting.");
+            thread::sleep(time::Duration::from_secs(1));
+        }
+    }
 }
